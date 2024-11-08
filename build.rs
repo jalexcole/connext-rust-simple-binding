9f5f9d0ee7032567a02f8917cb2aa41dba9f0f83
Borja Garcia <bgarcia@rti.com>
2024-11-08 09:43:16 +0100

    Uses an XML config. Automates the rtiddsgen and build

--- build.rs
+++ build.rs
@@ -1,14 +1,20 @@
 use std::env;
 use std::path::Path;
+use std::process::Command;
 
 const NDDSHOME_ENVVAR: &'static str = "NDDSHOME";
 const CONNEXTDDS_ARCH_ENVVAR: &'static str = "CONNEXTDDS_ARCH";
 const ARCHSTR_DEFAULT: &'static str = "x64Linux4gcc7.3.0";
+const IDL: &'static str = "res/hello.idl";
 
 fn main() {
+    println!("cargo::rerun-if-changed={}", IDL);
+    
     // Read the environment variables
-    let nddshome = env::var(NDDSHOME_ENVVAR).expect(&format!("{NDDSHOME_ENVVAR} envvar is not set"));
+    let nddshome =
+        env::var(NDDSHOME_ENVVAR).expect(&format!("{NDDSHOME_ENVVAR} envvar is not set"));
     let archstr = env::var(CONNEXTDDS_ARCH_ENVVAR).unwrap_or(ARCHSTR_DEFAULT.to_owned());
+    let outdir = env::var("OUT_DIR").unwrap();
 
     // Ensure the path is valid
     let nddshome = Path::new(&nddshome)
@@ -18,7 +24,41 @@ fn main() {
         panic!("The path specified in {NDDSHOME_ENVVAR} does not exist");
     }
 
-    println!("cargo:rustc-link-lib=dylib=nddscd");
-    println!("cargo:rustc-link-search=native={}/lib/{archstr}", nddshome.display());
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}/lib/{archstr}", nddshome.display());
+    // Run rtiddsgen
+    if !Command::new(format!("{}/bin/rtiddsgen", nddshome.display()))
+        .arg("-language")
+        .arg("C")
+        .arg("-d")
+        .arg(&outdir)
+        .arg(IDL)
+        .status()
+        .expect("rtiddsgen command failed to start")
+        .success()
+    {
+        panic!("rtiddsgen failed to execute")
+    }
+
+    // Build the generated files
+    cc::Build::new()
+        .include(format!("{}/include", nddshome.display()))
+        .include(format!("{}/include/ndds", nddshome.display()))
+        .include(&outdir)
+        .define("RTI_LINUX", None)
+        .define("RTI_UNIX", None)
+        .file(format!("{outdir}/hello.c"))
+        .file(format!("{outdir}/helloPlugin.c"))
+        .file(format!("{outdir}/helloSupport.c"))
+        .compile("libtypesupport.a");
+
+    // Add DDS libraries
+    println!("cargo::rustc-link-lib=dylib=nddscd");
+    println!("cargo::rustc-link-lib=dylib=nddscored");
+    println!(
+        "cargo::rustc-link-search=native={}/lib/{archstr}",
+        nddshome.display()
+    );
+    println!(
+        "cargo::rustc-link-arg=-Wl,-rpath,{}/lib/{archstr}",
+        nddshome.display()
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,44 @@ struct DDS_DomainParticipantFactory {
 }
 #[repr(C)]
 #[allow(non_camel_case_types)]
-struct DDS_DomainParticipantListener {
+struct DDS_DomainParticipant {
     _private: [u8; 0],
 }
 #[repr(C)]
 #[allow(non_camel_case_types)]
-struct DDS_DomainParticipant {
+struct DDS_DataWriter {
     _private: [u8; 0],
 }
+#[repr(C)]
+#[allow(non_camel_case_types)]
+struct HelloWorld {
+    msg: [i8; 256],
+}
 
-const DDS_STATUS_MASK_NONE: u32 = 0;
 const DDS_RETCODE_OK: i32 = 0;
 
 extern "C" {
     fn DDS_DomainParticipantFactory_get_instance() -> *mut DDS_DomainParticipantFactory;
-    fn DDS_DomainParticipantFactory_create_participant_with_profile(
+    fn HelloWorldTypeSupport_register_type(
+        participant: *mut DDS_DomainParticipant,
+        type_name: *const i8,
+    ) -> i32;
+    fn DDS_DomainParticipantFactory_register_type_support(
+        DDS_DomainParticipantFactory: *mut DDS_DomainParticipantFactory,
+        DDS_DomainParticipantFactory_RegisterTypeFunction: unsafe extern "C" fn(
+            *mut DDS_DomainParticipant,
+            *const i8,
+        ) -> i32,
+        type_name: *const i8,
+    ) -> i32;
+    fn DDS_DomainParticipantFactory_create_participant_from_config(
         factory: *mut DDS_DomainParticipantFactory,
-        domain_id: i32,
-        library_name: *const i8,
-        profile_name: *const i8,
-        participant_listener: *const DDS_DomainParticipantListener,
-        status_mask: u32,
+        configuration_name: *const i8,
     ) -> *mut DDS_DomainParticipant;
+    fn DDS_DomainParticipant_lookup_datawriter_by_name(
+        participant: *mut DDS_DomainParticipant,
+        datawriter_full_name: *const i8,
+    ) -> *mut DDS_DataWriter;
     fn DDS_DomainParticipant_delete_contained_entities(
         participant: *mut DDS_DomainParticipant,
     ) -> i32;
@@ -38,8 +54,6 @@ extern "C" {
 }
 
 fn main() {
-    let domain_id: i32 = 0;
-
     let factory = unsafe { DDS_DomainParticipantFactory_get_instance() };
 
     dbg!("factory address: {:?}", factory);
@@ -47,17 +61,23 @@ fn main() {
         panic!("Failed to get DomainParticipantFactory instance");
     }
 
-    let library_name = std::ffi::CString::new("MyLibrary").unwrap();
-    let profile_name = std::ffi::CString::new("MyProfile").unwrap();
-
-    let participant = unsafe {
-        DDS_DomainParticipantFactory_create_participant_with_profile(
+    let type_name = std::ffi::CString::new("HelloWorld").unwrap();
+    let retcode = unsafe {
+        DDS_DomainParticipantFactory_register_type_support(
             factory,
-            domain_id,
-            library_name.as_ptr(),
-            profile_name.as_ptr(),
-            std::ptr::null(),
-            DDS_STATUS_MASK_NONE,
+            HelloWorldTypeSupport_register_type,
+            type_name.as_ptr(),
+        )
+    };
+    if retcode != DDS_RETCODE_OK {
+        panic!("Failed to register type: {retcode}");
+    }
+
+    let participant_name = std::ffi::CString::new("MyParticipantLibrary::MyParticipant").unwrap();
+    let participant = unsafe {
+        DDS_DomainParticipantFactory_create_participant_from_config(
+            factory,
+            participant_name.as_ptr(),
         )
     };
 
@@ -66,6 +86,17 @@ fn main() {
         panic!("Failed to create DomainParticipant");
     }
 
+    let datawriter_name = std::ffi::CString::new("MyPublisher::MyWriter").unwrap();
+    let datawriter = unsafe {
+        DDS_DomainParticipant_lookup_datawriter_by_name(participant, datawriter_name.as_ptr())
+    };
+
+    dbg!("datawriter address: {:?}", datawriter);
+    if datawriter == std::ptr::null_mut() {
+        panic!("Failed to create DataWriter");
+    }
+
+    // Teardown
     let retcode = unsafe { DDS_DomainParticipant_delete_contained_entities(participant) };
     if retcode != DDS_RETCODE_OK {
         panic!("Failed to delete contained entities: {retcode}");
